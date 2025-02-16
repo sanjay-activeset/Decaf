86001d59be44208a2343f76d65dc6b8cf03ff981
Rehan Khurshid <rehankhurshid1@gmail.com>
2025-02-16 13:20:30 +0530

    Updating the Homepage JS

--- decafabout.js
+++ decafabout.js
@@ -225,13 +225,20 @@ const split2type = document.querySelectorAll("[text='highlight2']");
 split2type.forEach((char) => {
   const text = new SplitType(char, { types: ["lines", "words"] });
 
+  console.log("Lines:", text.lines); // Debugging
+
+  if (!text.lines || text.lines.length === 0) {
+    console.warn("SplitType failed on:", char);
+    return;
+  }
+
   gsap.from(text.lines, {
     scrollTrigger: {
       trigger: char,
       start: "top 50%",
       end: "bottom 50%",
       scrub: true,
-      markers: false,
+      markers: true, // Debugging
     },
     opacity: 0.2,
     stagger: 0.1,
