5dedf7fe8a62a04df81ceb26e27493e8e583332e
Rehan Khurshid <rehankhurshid1@gmail.com>
2025-02-16 13:11:45 +0530

    Updating the Homepage JS

--- decafabout.js
+++ decafabout.js
@@ -221,20 +221,22 @@ gsap.from(".footer_right-content-image", {
   },
 });
 
-const split2type = document.querySelectorAll("[text='highlight2']");
-split2type.forEach((char) => {
-  const text = new SplitType(char, { types: ["lines", "words"] });
+setTimeout(() => {
+  const split2type = document.querySelectorAll("[text='highlight2']");
+  split2type.forEach((char) => {
+    const text = new SplitType(char, { types: ["lines", "words"] });
 
-  gsap.from(text.lines, {
-    scrollTrigger: {
-      trigger: char,
-      start: "top 50%",
-      end: "bottom 50%",
-      scrub: true,
-      markers: false,
-    },
-    opacity: 0.2,
-    stagger: 0.1,
-    ease: "expoScale.out",
+    gsap.from(text.lines, {
+      scrollTrigger: {
+        trigger: char,
+        start: "top 50%",
+        end: "bottom 50%",
+        scrub: true,
+        markers: false,
+      },
+      opacity: 0.2,
+      stagger: 0.1,
+      ease: "expoScale.out",
+    });
   });
-});
+}, 100);
