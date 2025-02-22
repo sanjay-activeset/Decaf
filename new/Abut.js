104d20e5d4b1b3ad8176d8f5895f393f9ef796c4
Rehan Khurshid <rehankhurshid1@gmail.com>
2025-02-22 15:15:42 +0530

    updated

--- new/Abut.js
+++ new/Abut.js
@@ -209,3 +209,15 @@ gsap.from(".footer_right-content-image", {
     toggleActions: "play none none none",
   },
 });
+gsap.from("[Image1='Animation']", {
+  x: "-100%",
+  y: "-100%",
+  opacity: 0,
+  duration: 1,
+  ease: "power2.out",
+  scrollTrigger: {
+    trigger: "[Image1=Animation]",
+    start: "top 80%",
+    toggleActions: "play none none none",
+  },
+});
