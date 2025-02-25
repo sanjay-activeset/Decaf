1f908912e11894feec8b7870ee4a2868ea045488
Rehan Khurshid <rehankhurshid1@gmail.com>
2025-02-25 18:56:52 +0530

    L2

--- decafbusiness.js
+++ decafbusiness.js
@@ -405,7 +405,7 @@ function visa() {
 
     form
       .to(".is--form-top", { y: "50%" }, "+=1.5")
-      .to(".is--form-top", { y: "0%", scale: 1 }, "+=1.5")
+      .to(".is--form-top", { y: "0%", scale: 1, duration: 1 }, "+=1.5")
       .to(".is--sign-1", { opacity: 0.2 })
       .to(".sign-in_simple", { opacity: 1 })
       .to(".sign-in_simple-text-1", { opacity: 0.2 })
