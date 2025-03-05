0eb668193cdba84e4ffab839643b4ebb5965184e
Rehan Khurshid <rehankhurshid1@gmail.com>
2025-03-05 10:21:48 +0530

    L2

--- decafscroll.js
+++ decafscroll.js
@@ -8,85 +8,98 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function heroAnim() {
-    // Apply SplitType to all elements once
-    const splitHeroPara1 = new SplitType("[hero='word']", { types: "lines" });
-    const splitHeroline1 = new SplitType("[hero='line']", { types: "lines" });
-    const splitHeroPara2 = new SplitType("[hero2='word']", { types: "lines" });
-    const splitHeroline2 = new SplitType("[hero2='line']", { types: "lines" });
-    const splitHeroPara3 = new SplitType("[hero3='word']", { types: "lines" });
-    const splitHeroline3 = new SplitType("[hero3='line']", { types: "lines" });
-    const splitHeroPara4 = new SplitType("[hero4='word']", { types: "lines" });
-    const splitHeroline4 = new SplitType("[hero4='line']", { types: "lines" });
-
-    document.querySelectorAll("[hero='line'], [hero2='line']").forEach((el) => {
-      el.style.display = "block";
-      el.style.position = "relative";
-    });
-
-    const heroAnimTL = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".section_home",
-        start: "top top",
-        end: "bottom bottom",
-        scrub: true,
-      },
-      defaults: { duration: 1, ease: "none" },
-    });
-
-    heroAnimTL.to(".home_your-content-wrapper", { opacity: 0 });
-
-    // Check if the screen width is mobile (e.g., max-width: 768px)
-    if (window.matchMedia("(max-width: 768px)").matches) {
-      heroAnimTL.to(".home_your-bg-image", {
-        scale: 0.9,
-        borderRadius: "24px",
+    // Run only if screen width is greater than 768px (desktop)
+    if (window.matchMedia("(min-width: 769px)").matches) {
+      // Apply SplitType to all elements once
+      const splitHeroPara1 = new SplitType("[hero='word']", { types: "lines" });
+      const splitHeroline1 = new SplitType("[hero='line']", { types: "lines" });
+      const splitHeroPara2 = new SplitType("[hero2='word']", {
+        types: "lines",
       });
-    } else {
+      const splitHeroline2 = new SplitType("[hero2='line']", {
+        types: "lines",
+      });
+      const splitHeroPara3 = new SplitType("[hero3='word']", {
+        types: "lines",
+      });
+      const splitHeroline3 = new SplitType("[hero3='line']", {
+        types: "lines",
+      });
+      const splitHeroPara4 = new SplitType("[hero4='word']", {
+        types: "lines",
+      });
+      const splitHeroline4 = new SplitType("[hero4='line']", {
+        types: "lines",
+      });
+
+      document
+        .querySelectorAll("[hero='line'], [hero2='line']")
+        .forEach((el) => {
+          el.style.display = "block";
+          el.style.position = "relative";
+        });
+
+      const heroAnimTL = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".section_home",
+          start: "top top",
+          end: "bottom bottom",
+          scrub: true,
+        },
+        defaults: { duration: 1, ease: "none" },
+      });
+
+      heroAnimTL.to(".home_your-content-wrapper", { opacity: 0 });
+
       heroAnimTL.to(
         ".home_your-bg-image",
         { width: "23rem", height: "760px", duration: 2.5 },
         "+=3"
       );
-    }
 
-    heroAnimTL
-      .to(".home_your-bg-image", { opacity: 0.2 })
-      .to(".home_send", { opacity: 1 })
-      .to(".home_send-h1", { opacity: 0.2 })
-      .to(".home_send-h2", { opacity: 1 })
-      .to(".home_send", { opacity: 0 })
-      .to(".home_your-bg-image", { opacity: 0 })
-      .to(".home_your-app", { opacity: 1 })
-      .from(
-        splitHeroPara1.lines,
-        { yPercent: 100, opacity: 0, stagger: 0.5 },
-        "-=0.5"
-      )
-      .from(splitHeroline1.lines, { yPercent: 100, opacity: 0, stagger: 0.5 })
-      .to(".home_your-app", { opacity: 0 }, "+=1.5")
-      .to(".home_your-set", { opacity: 1 })
-      .from(
-        splitHeroPara2.lines,
-        { yPercent: 100, opacity: 0, stagger: 0.5 },
-        "-=0.5"
-      )
-      .from(splitHeroline2.lines, { yPercent: 100, opacity: 0, stagger: 0.5 })
-      .to(".home_your-set", { opacity: 0 }, "+=1.5")
-      .to(".home_your-make", { opacity: 1 })
-      .from(
-        splitHeroPara3.lines,
-        { yPercent: 100, opacity: 0, stagger: 0.5 },
-        "-=0.5"
-      )
-      .from(splitHeroline3.lines, { yPercent: 100, opacity: 0, stagger: 0.5 })
-      .to(".home_your-make", { opacity: 0 }, "+=1.5")
-      .to(".home_your-trans", { opacity: 1 })
-      .from(
-        splitHeroPara4.lines,
-        { yPercent: 100, opacity: 0, stagger: 0.5 },
-        "-=0.5"
-      )
-      .from(splitHeroline4.lines, { yPercent: 100, opacity: 0, stagger: 0.5 });
+      heroAnimTL
+        .to(".home_your-bg-image", { opacity: 0.2 })
+        .to(".home_send", { opacity: 1 })
+        .to(".home_send-h1", { opacity: 0.2 })
+        .to(".home_send-h2", { opacity: 1 })
+        .to(".home_send", { opacity: 0 })
+        .to(".home_your-bg-image", { opacity: 0 })
+        .to(".home_your-app", { opacity: 1 })
+        .from(
+          splitHeroPara1.lines,
+          { yPercent: 100, opacity: 0, stagger: 0.5 },
+          "-=0.5"
+        )
+        .from(splitHeroline1.lines, { yPercent: 100, opacity: 0, stagger: 0.5 })
+        .to(".home_your-app", { opacity: 0 }, "+=1.5")
+        .to(".home_your-set", { opacity: 1 })
+        .from(
+          splitHeroPara2.lines,
+          { yPercent: 100, opacity: 0, stagger: 0.5 },
+          "-=0.5"
+        )
+        .from(splitHeroline2.lines, { yPercent: 100, opacity: 0, stagger: 0.5 })
+        .to(".home_your-set", { opacity: 0 }, "+=1.5")
+        .to(".home_your-make", { opacity: 1 })
+        .from(
+          splitHeroPara3.lines,
+          { yPercent: 100, opacity: 0, stagger: 0.5 },
+          "-=0.5"
+        )
+        .from(splitHeroline3.lines, { yPercent: 100, opacity: 0, stagger: 0.5 })
+        .to(".home_your-make", { opacity: 0 }, "+=1.5")
+        .to(".home_your-trans", { opacity: 1 })
+        .from(
+          splitHeroPara4.lines,
+          { yPercent: 100, opacity: 0, stagger: 0.5 },
+          "-=0.5"
+        )
+        .from(splitHeroline4.lines, {
+          yPercent: 100,
+          opacity: 0,
+          stagger: 0.5,
+        });
+    }
   }
 
   heroAnim();
