b8a5fdd783dfbb7e238d7e68717f1df2cf7c1bfd
Rehan Khurshid <rehankhurshid1@gmail.com>
2025-03-05 10:07:32 +0530

    L2

--- decafscroll.js
+++ decafscroll.js
@@ -713,3 +713,76 @@ gsap.from(".footer_right-content-image", {
     toggleActions: "play none none none",
   },
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Register GSAP plugins
+  gsap.registerPlugin(ScrollTrigger);
+
+  function heroAnim() {
+    // Apply SplitType to all elements once
+    const splitHeroPara1 = new SplitType("[hero='word']", { types: "lines" });
+    const splitHeroline1 = new SplitType("[hero='line']", { types: "lines" });
+    const splitHeroPara2 = new SplitType("[hero2='word']", { types: "lines" });
+    const splitHeroline2 = new SplitType("[hero2='line']", { types: "lines" });
+    const splitHeroPara3 = new SplitType("[hero3='word']", { types: "lines" });
+    const splitHeroline3 = new SplitType("[hero3='line']", { types: "lines" });
+    const splitHeroPara4 = new SplitType("[hero4='word']", { types: "lines" });
+    const splitHeroline4 = new SplitType("[hero4='line']", { types: "lines" });
+
+    document.querySelectorAll("[hero='line'], [hero2='line']").forEach((el) => {
+      el.style.display = "block";
+      el.style.position = "relative";
+    });
+
+    const heroAnimTL = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".home_your-comp-wrapper",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: true,
+      },
+      defaults: { duration: 1, ease: "none" },
+    });
+
+    heroAnimTL.to(".home_your-content-wrapper", { opacity: 0 });
+
+    heroAnimTL.to(
+      ".home_your-bg-image",
+      { width: "23rem", height: "760px", duration: 2.5 },
+      "+=3"
+    );
+
+    heroAnimTL
+      .to(".home_your-bg-image", { opacity: 0.2 })
+      .to(".home_send", { opacity: 1 })
+      .to(".home_send", { opacity: 0 })
+
+      .from(
+        splitHeroPara1.lines,
+        { yPercent: 100, opacity: 0, stagger: 0.5 },
+        "-=0.5"
+      )
+      .from(splitHeroline1.lines, { yPercent: 100, opacity: 0, stagger: 0.5 })
+
+      .from(
+        splitHeroPara2.lines,
+        { yPercent: 100, opacity: 0, stagger: 0.5 },
+        "-=0.5"
+      )
+      .from(splitHeroline2.lines, { yPercent: 100, opacity: 0, stagger: 0.5 })
+
+      .from(
+        splitHeroPara3.lines,
+        { yPercent: 100, opacity: 0, stagger: 0.5 },
+        "-=0.5"
+      )
+      .from(splitHeroline3.lines, { yPercent: 100, opacity: 0, stagger: 0.5 })
+
+      .from(
+        splitHeroPara4.lines,
+        { yPercent: 100, opacity: 0, stagger: 0.5 },
+        "-=0.5"
+      )
+      .from(splitHeroline4.lines, { yPercent: 100, opacity: 0, stagger: 0.5 });
+  }
+});
