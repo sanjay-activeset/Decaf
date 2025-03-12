92bd8b0dc97763f8785a6f41d3ce56a54ec12ba7
Rehan Khurshid <rehankhurshid1@gmail.com>
2025-03-12 16:21:35 +0530

    L2

--- decafscroll.js
+++ decafscroll.js
@@ -94,7 +94,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const lines2 = prepareText("[home2=heading]");
   const home3 = document.querySelector("[home3=heading]");
 
-  let tl = gsap.timeline({ delay: 4 });
+  let tl = gsap.timeline({ delay: 3.5 });
 
   tl.from(
     ".home_your-logo-image",
