b1e1e9270a469f285e3969b1aa67bd325c0d81c5
Rehan Khurshid <rehankhurshid1@gmail.com>
2025-03-18 10:31:19 +0530

    new

--- decafscroll.js
+++ decafscroll.js
@@ -255,6 +255,40 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
+  cardAnimTL
+    .to(".decaf_visa-card", { opacity: 0.2 })
+    .to(".decaf_every", { opacity: 1 })
+    .to(".decaf_every-content-h-1", { opacity: 0.2 })
+    .to(".decaf_every-content-h-2", { opacity: 1 })
+    .to(".decaf_every", { opacity: 0 })
+    .to(".decaf_wallet", { opacity: 1 })
+    .to(".decaf_wallet-content-h-1", { opacity: 0.2 })
+    .to(".decaf_wallet-content-h-2", { opacity: 1 });
+  const animations = [
+    {
+      headingsAttr: "Heading11",
+      textLinesAttr: "Textline11",
+      trigger: ".send_content",
+    },
+    {
+      headingsAttr: "Heading12",
+      textLinesAttr: "Textline12",
+      trigger: ".manage-component",
+    },
+    {
+      headingsAttr: "Heading13",
+      textLinesAttr: "Textline13",
+      trigger: ".grow-component",
+    },
+    {
+      headingsAttr: "Heading14",
+      textLinesAttr: "Textline14",
+      trigger: ".receive-component",
+    },
+  ];
+
+  let timelines = {}; // Store all timelines
+
   checkAndRunAnimation();
   window.addEventListener("resize", checkAndRunAnimation);
 });
