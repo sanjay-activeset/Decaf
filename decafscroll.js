dc745d9fe48f4b2d7f43673f1e2f1e1d7d233836
Rehan Khurshid <rehankhurshid1@gmail.com>
2025-03-18 10:50:53 +0530

    new

--- decafscroll.js
+++ decafscroll.js
@@ -77,34 +77,32 @@ document.addEventListener("DOMContentLoaded", function () {
     heroAnimTL.to(
       ".home_your-content-wrapper",
       { opacity: 0, duration: 2.5, ease: "power2.out" },
-      "2"
+      "+=2"
     );
 
     heroAnimTL.to(
       ".home_your-bg-image",
       {
         width: "23rem",
-        height: "760px",
+        height: "48rem",
         duration: 2.5,
-        ease: "power1.inOut",
       },
-      "2"
-    );
-
-    heroAnimTL
-      .to(".home_your-bg-image", { opacity: 0.2 }, "2")
-      .to(".home_send", { opacity: 1 })
-      .to(".home_send-h1", { opacity: 0.2 })
-      .to(".home_send-h2", { opacity: 1 })
-      .to(".home_send", { opacity: 0 })
-      .to(".home_your-bg-image", { opacity: 0 })
-      .to(".home_your-app", { opacity: 1 })
-      .to(".home_your-app", { opacity: 0 }, "+=1")
-      .to(".home_your-set", { opacity: 1 })
-      .to(".home_your-set", { opacity: 0 }, "+=1")
-      .to(".home_your-make", { opacity: 1 })
-      .to(".home_your-make", { opacity: 0 }, "+=1")
-      .to(".home_your-trans", { opacity: 1 });
+      "0"
+    ),
+      heroAnimTL
+        .to(".home_your-bg-image", { opacity: 0.2 })
+        .to(".home_send", { opacity: 1 })
+        .to(".home_send-h1", { opacity: 0.2 })
+        .to(".home_send-h2", { opacity: 1 })
+        .to(".home_send", { opacity: 0 })
+        .to(".home_your-bg-image", { opacity: 0 })
+        .to(".home_your-app", { opacity: 1 })
+        .to(".home_your-app", { opacity: 0 }, "+=1")
+        .to(".home_your-set", { opacity: 1 })
+        .to(".home_your-set", { opacity: 0 }, "+=1")
+        .to(".home_your-make", { opacity: 1 })
+        .to(".home_your-make", { opacity: 0 }, "+=1")
+        .to(".home_your-trans", { opacity: 1 });
   }
 
   function checkAndRunAnimation() {
