ab8f0d15c8d3693eddc673306cc9ed9f3c7b40f6
Rehan Khurshid <rehankhurshid1@gmail.com>
2025-03-18 14:13:56 +0530

    new

--- decafscroll.js
+++ decafscroll.js
@@ -77,24 +77,21 @@ document.addEventListener("DOMContentLoaded", function () {
       defaults: { duration: 1, ease: "none" },
     });
 
-    heroAnimTL.to(
-      ".home_your-content-wrapper",
-      { opacity: 0, duration: 1.5 },
-      "+=1"
-    );
+    heroAnimTL.to(".home_your-content-wrapper", { opacity: 0, duration: 1.5 });
 
     heroAnimTL.to(
       ".home_your-bg-image",
       {
         width: "23rem",
         height: "80vh",
-        duration: 2,
+        duration: 1,
       },
       "<"
     );
+    heroAnimTL.to(".home_your-bg-image", { opacity: 0.2 }, "<");
 
     heroAnimTL
-      .to(".home_your-bg-image", { opacity: 0.2 }, "<")
+
       .to(".home_send", { opacity: 1 })
       .to(".home_send-h1", { opacity: 0.2 })
       .to(".home_send-h2", { opacity: 1 })
