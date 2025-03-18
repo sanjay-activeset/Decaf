1d4c3ed4117eb894c9a14f50a13bc58ed37d1b62
Rehan Khurshid <rehankhurshid1@gmail.com>
2025-03-18 17:04:20 +0530

    new

--- Flexxi-home.js
+++ Flexxi-home.js
@@ -1,18 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
   gsap.registerPlugin(ScrollTrigger);
 
-  const cardAnimTL = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".latest_head-wrapper",
-      start: "top top",
-      end: "bottom bottom",
-      scrub: true,
-    },
-    defaults: { ease: "none" },
-  });
-
-  cardAnimTL.fromTo(".latest_heading", { scale: 1.2 }, { scale: 0.7 });
-  cardAnimTL.to(".work_wrapper", {
-    y: "-40vh",
-  });
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".latest_head-wrapper",
+        start: "top 0%",
+        end: "bottom bottom",
+        scrub: true,
+      },
+      defaults: { ease: "none" },
+    })
+    .to(".latest_heading", {
+      scale: 0.7,
+    })
+    .to(
+      " .work_wrapper",
+      {
+        marginTop: "-40vh",
+      },
+      "<"
+    );
 });
