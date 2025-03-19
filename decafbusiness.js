e5099340e9916cc72fbfff2c90ca29f5302f28f8
Rehan Khurshid <rehankhurshid1@gmail.com>
2025-03-19 13:03:28 +0530

    new

--- decafbusiness.js
+++ decafbusiness.js
@@ -205,7 +205,7 @@ document.addEventListener("DOMContentLoaded", function () {
         // .to(".sign-in_simple-text-2", { opacity: 1, duration: 0.7 })
         // .to(".sign-in_simple-text-2", { opacity: 0.2, duration: 0.3 })
         // .to(".sign-in_simple-text-3", { opacity: 1, duration: 0.7 })
-        .to(".sign-in_simple", { opacity: 0, duration: 0.3 })
+        .to(".sign-in_simple", { opacity: 0, duration: 0.1 })
         .to(".is--sign-1", { opacity: 1, duration: 0.3 })
         .to("[signform='image']", { x: "-10%", width: "70%" })
         .to(".is--sign-contenr1", { opacity: 1, duration: 0.3 })
